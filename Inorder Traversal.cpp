33c7e4d549d271f49459ca96ec83ff1ded782ca1
ankita <94631478+Ankita3503@users.noreply.github.com>
2022-11-11 20:38:42 +0530

    Create Inorder Traversal.cpp

---
+++ Inorder Traversal.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+  public:
+      void in(Node* root ,vector<int> &ans)
+      {
+         if(root==NULL)
+         {
+             return ;
+         }
+         
+             in(root->left,ans);
+             ans.push_back(root->data);
+             in(root->right,ans);
+           
+             
+      }
+   
+    vector<int> inOrder(Node* root) {
+         vector<int>ans;
+         
+         in(root,ans);
+         return ans;
+    }
+};
